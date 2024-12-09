519247982c0651f6bcd063120cb78f2f28b119da
dMario24 <data.mario24@gmail.com>
2024-12-09 17:08:05 +0900

    NEXT_PUBLIC_BASE_URL

--- app/lib/getFlags.ts
+++ app/lib/getFlags.ts
@@ -5,8 +5,8 @@ export type Flag = {
 };
 
 export async function getFlags(): Promise<Flag[]> {
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/flags`, { cache: 'no-store' });
-  const response = await fetch(`http://localhost:3000/api/flags`, { cache: 'no-store' });
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/flags`, { cache: 'no-store' });
+  // const response = await fetch(`http://localhost:3000/api/flags`, { cache: 'no-store' });
   if (!response.ok) {
     throw new Error('Failed to fetch flags');
   }
