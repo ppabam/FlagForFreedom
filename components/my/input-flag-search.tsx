60bf23cc28b97699edded23897fa927a17b8b33a
dMario24 <data.mario24@gmail.com>
2024-12-11 15:16:02 +0900

    input lang ko

--- components/my/input-flag-search.tsx
+++ components/my/input-flag-search.tsx
@@ -3,6 +3,6 @@ import { Input } from "@/components/ui/input"
 
 export function InputFlagSearch() {
   return (
-    <Input placeholder="야발라바하야 야발라바탄핵" />
+    <Input placeholder="야발라바하야 야발라바탄핵" lang="ko" />
   )
 }
