96ea6788ceade6e9725525d8b55d6fed7c64cfac
dMario24 <data.mario24@gmail.com>
2024-12-11 13:14:55 +0900

    Error: 'useState' is defined but never used

--- components/my/button-upload.tsx
+++ components/my/button-upload.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { ImageUp } from "lucide-react";
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/hooks/use-toast"
 
