2b4de56106ebd04134845ae0c83d8cf046e853b6
dMario24 <data.mario24@gmail.com>
2024-12-13 14:51:06 +0900

    export const revalidate = 60;

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getFlags } from '@/app/lib/getFlags';
 import Flags from '@/components/my/flags';
 import { FooterFlags } from '@/components/my/footer-flags';
 
+export const revalidate = 60;
+
 export default async function Home() {
   const flags = await getFlags();
   return (
