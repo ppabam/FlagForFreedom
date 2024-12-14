7fcefcda5629d69d3344f7fdf19701097c674a29
dMario24 <data.mario24@gmail.com>
2024-12-14 15:25:45 +0900

    Fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import { getFlags } from "@/app/lib/getFlags";
+import { getFlags } from "@/app/lib/getFlags";
 import { fetchFlags } from "@/app/lib/data";
 import FlagsPage from "@/components/my/flags-page";
 import { FooterFlags } from "@/components/my/footer-flags";
@@ -6,16 +6,16 @@ import { FooterFlags } from "@/components/my/footer-flags";
 export const fetchCache = 'force-no-store';
 
 export default async function Home() {
-  // let flags;
+  let flags;
 
-  // try {
-  //   flags = await fetchFlags();
-  // } catch (error) {
-  //   console.error('fetchFlags failed, using getFlags as fallback:', error);
-  //   flags = await getFlags(); // Fallback to getFlags if fetchFlags fails
-  // }
+  try {
+    flags = await fetchFlags();
+  } catch (error) {
+    console.error('fetchFlags failed, using getFlags as fallback:', error);
+    flags = await getFlags(); // Fallback to getFlags if fetchFlags fails
+  }
 
-  const flags = await fetchFlags();
+  // const flags = await fetchFlags();
 
   return (
     <>
