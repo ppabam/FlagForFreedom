a3847d7766f3236dcfb269ebcc1b9a6059e79206
dMario24 <data.mario24@gmail.com>
2024-12-14 13:02:12 +0900

    unuse getFlags

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getFlags } from "@/app/lib/getFlags";
+// import { getFlags } from "@/app/lib/getFlags";
 import { fetchFlags } from "@/app/lib/data";
 import Flags from "@/components/my/flags";
 import { FooterFlags } from "@/components/my/footer-flags";
