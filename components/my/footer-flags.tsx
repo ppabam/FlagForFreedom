ab2c5cd98296adf6ef45d2ab91daf4cebd50ea53
dMario24 <data.mario24@gmail.com>
2024-12-14 15:32:31 +0900

    집회 준비와 위기 시 행동 요령

--- components/my/footer-flags.tsx
+++ components/my/footer-flags.tsx
@@ -1,5 +1,12 @@
-import { Toilet, Flag, Flame, Angry, Smartphone, GitPullRequestCreateArrow } from "lucide-react"
-import Link from 'next/link';
+import {
+  Toilet,
+  Flag,
+  Flame,
+  Angry,
+  Smartphone,
+  GitPullRequestCreateArrow,
+} from "lucide-react";
+import Link from "next/link";
 
 export function FooterFlags({ copyrightHref = "/" }) {
   return (
@@ -11,23 +18,55 @@ export function FooterFlags({ copyrightHref = "/" }) {
               <Flag size={24} />
             </Link>
           )}
-          <a href="https://www.mapplerk3.com/minjumap" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-800">
+          <a
+            href="https://www.mapplerk3.com/minjumap"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-800"
+          >
             <Toilet size={24} />
           </a>
-          <a href="https://candle.btsroad.com" target="_blank" rel="noopener noreferrer" className="text-pink-500 hover:text-pink-800">
+          <a
+            href="https://candle.btsroad.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-pink-500 hover:text-pink-800"
+          >
             <Flame size={24} />
           </a>
-          <a href="https://www.yna.co.kr/view/AKR20241212062700001?section=politics/all" target="_blank" rel="noopener noreferrer" className="text-purple-500 hover:text-purple-800">
+          <a
+            href="https://www.yna.co.kr/view/AKR20241212062700001?section=politics/all"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-purple-500 hover:text-purple-800"
+          >
             <Angry size={24} />
           </a>
-          <a href="https://nodong.org/notice/7872937" target="_blank" rel="noopener noreferrer" className="text-green-500 hover:text-green-800">
+          <a
+            href="https://nodong.org/notice/7872937"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-green-500 hover:text-green-800"
+          >
             <Smartphone size={24} />
           </a>
-          <a href="https://github.com/dMario24/flag123" target="_blank" rel="noopener noreferrer" className="text-yellow-500 hover:text-yellow-800">
+          <a
+            href="https://github.com/dMario24/flag123"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-yellow-500 hover:text-yellow-800"
+          >
             <GitPullRequestCreateArrow size={24} />
           </a>
         </div>
 
+        <p className="text-sm text-gray-600 mt-3 text-center">
+          {/* &copy; 12.3, <a href={copyrightHref} className="text-blue-500 hover:underline">어디서 도대체 무얼</a> */}
+          <Link href={"https://spurious-ziconium-e0f.notion.site/15474ccf5596800abecffbfb5c11fec5"} className="text-red-500 hover:underline">
+            집회 준비와 위기 시 행동 요령
+          </Link>
+        </p>
+        
         <p className="text-sm text-gray-600 mt-3 text-center">
           {/* &copy; 12.3, <a href={copyrightHref} className="text-blue-500 hover:underline">어디서 도대체 무얼</a> */}
           &copy; 12.3,
@@ -35,7 +74,9 @@ export function FooterFlags({ copyrightHref = "/" }) {
             어디서 도대체 무얼
           </Link>
         </p>
+
+        
       </div>
     </footer>
-  )
+  );
 }
