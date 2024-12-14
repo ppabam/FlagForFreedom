135cc62ac9677d1ab197c1bb18f702ad9be71ce5
dMario24 <data.mario24@gmail.com>
2024-12-14 15:07:34 +0900

    export const fetchCache = 'force-no-store'; -> route

--- app/api/flags/db/route.ts
+++ app/api/flags/db/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { fetchFlags } from '@/app/lib/data';
 
+export const fetchCache = 'force-no-store';
 
 export async function GET() {
   const data = await fetchFlags();
