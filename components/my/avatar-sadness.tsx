6a70b78b9411a920537ea24b13844c6e0be97f22
dMario24 <data.mario24@gmail.com>
2024-12-16 10:12:49 +0900

    resize sadness.webp 40x40

--- components/my/avatar-sadness.tsx
+++ components/my/avatar-sadness.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export function AvatarSadness() {
   return (
     <Avatar className="hover:animate-bounce">
-      <AvatarImage src="/sadness.webp" />
+      <AvatarImage src="/sadness_2_40x40.webp" />
       <AvatarFallback><Flag /></AvatarFallback>
     </Avatar>
   )
