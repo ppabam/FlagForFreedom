29557fbc8bd828053162b267ac368a5d5274a546
Soonoh <soonoh.jung@gmail.com>
2024-12-17 14:52:00 +0900

    fix: prevent analytics error if no measurementId defined

--- app/analytics.ts
+++ app/analytics.ts
@@ -17,7 +17,7 @@ export const createFirebaseApp = () => {
     // Check that `window` is in scope for the analytics module!
     if (typeof window !== "undefined") {
       // Enable analytics. https://firebase.google.com/docs/analytics/get-started
-      if ("measurementId" in clientCredentials) {
+      if (clientCredentials.measurementId) {
         getAnalytics();
       }
     }
