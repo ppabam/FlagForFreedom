5cda564ed5e254491bbe8e2eebbd566996517124
dMario24 <data.mario24@gmail.com>
2024-12-17 21:34:36 +0900

    unuse

--- components/my/flags-page.tsx
+++ components/my/flags-page.tsx
@@ -19,7 +19,7 @@ import { AvatarSadness } from '@/components/my/avatar-sadness';
 //   DialogTrigger,
 // } from "@/components/ui/dialog";
 // import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Label } from "@/components/ui/label";
 // import { Button } from "@/components/ui/button";
 import { 
   // Copy, MapPinned, ShieldOff, 
