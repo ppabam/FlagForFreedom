afe98ec071e285097440b25ad9345d179b78e34f
dMario24 <data.mario24@gmail.com>
2024-12-18 04:57:34 +0900

    source: /:path*(.webp|.jpg|.jpeg|.png|.gif|.svg)

--- next.config.mjs
+++ next.config.mjs
@@ -12,25 +12,10 @@ const nextConfig = {
   },
   output: "standalone", // docker
 
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/:path*", // Match all routes
-  //       headers: [
-  //         {
-  //           key: "Cache-Control",
-  //           value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-  //         },
-  //         { key: "Pragma", value: "no-cache" },
-  //         { key: "Expires", value: "0" },
-  //       ],
-  //     },
-  //   ];
-  // },
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/:path*(.webp|.jpg|.jpeg|.png|.gif|.svg)", // Apply cache control to image paths
         headers: [
           {
             key: "Cache-Control",
