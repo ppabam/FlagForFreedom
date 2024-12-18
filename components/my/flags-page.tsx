1fbcc1cbac7da43d99474ff0cc6bdc938171e227
dMario24 <data.mario24@gmail.com>
2024-12-18 22:00:19 +0900

    Drawer

--- components/my/flags-page.tsx
+++ components/my/flags-page.tsx
@@ -5,10 +5,66 @@ import Image from "next/image";
 
 import { InputFlagSearch } from "@/components/my/input-flag-search";
 import { ButtonUpload } from "@/components/my/button-upload";
-import { AvatarSadness } from "@/components/my/avatar-sadness";
+import * as React from "react";
+import { Minus, Plus } from "lucide-react";
+import { Bar, BarChart, ResponsiveContainer } from "recharts";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+];
 
 import {
-  Heart, 
+  Heart,
   Info,
   Menu,
   // Barcode,
@@ -21,13 +77,13 @@ import {
   Images,
   MapPinned,
   Github,
-  Shuffle
+  Shuffle,
 } from "lucide-react";
 import { parseCookies, setCookie } from "nookies"; // nookies 사용
 
 import { Flag } from "@/app/lib/definitions"; // Flag 타입을 가져옵니다.
 
-import { getAuthHeaders } from "@/lib/utils"
+import { getAuthHeaders } from "@/lib/utils";
 
 import {
   DropdownMenu,
@@ -37,7 +93,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 interface FlagsProps {
   initialFlags: Flag[];
@@ -49,13 +105,18 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredFlags, setFilteredFlags] = useState<Flag[]>(initialFlags);
   const [likedFlags, setLikedFlags] = useState<string[]>([]); // 좋아요된 플래그 ID 배열
-  const [animatingFlags, setAnimatingFlags] = useState<{ [key: string]: boolean }>({}); // 개별 플래그 애니메이션 상태
+  const [animatingFlags, setAnimatingFlags] = useState<{
+    [key: string]: boolean;
+  }>({}); // 개별 플래그 애니메이션 상태
 
   const shuffleFlags = () => {
     const shuffledFlags = [...filteredFlags];
     for (let i = shuffledFlags.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledFlags[i], shuffledFlags[j]] = [shuffledFlags[j], shuffledFlags[i]];
+      [shuffledFlags[i], shuffledFlags[j]] = [
+        shuffledFlags[j],
+        shuffledFlags[i],
+      ];
     }
     setFilteredFlags(shuffledFlags);
   };
@@ -94,7 +155,9 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
         return {
           ...flag,
           like_count:
-            likeStatus === 1 ? Number(flag.like_count) + 1 : Number(flag.like_count) - 1,
+            likeStatus === 1
+              ? Number(flag.like_count) + 1
+              : Number(flag.like_count) - 1,
         };
       }
       return flag;
@@ -151,6 +214,11 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
     setFilteredFlags(filtered);
   };
 
+  const [goal, setGoal] = React.useState(350);
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -159,11 +227,10 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
         <div className="container mx-auto flex items-center px-4 py-3 space-x-4">
           {/* Logo */}
           <div className="flex items-center space-x-2">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild >
+            {/* <DropdownMenu>
+              <DropdownMenuTrigger asChild>
                 <Menu
                   size={33}
-                  // className="text-red-500"
                 />
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
@@ -183,7 +250,6 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
                     <Shuffle />
                     <span>좋아요 뒤죽박죽</span>
                   </DropdownMenuItem>
-
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
@@ -215,7 +281,12 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
                   <Images />
                   <span>깃발 모아 보기</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => window.location.href = 'https://github.com/dMario24/flag123?tab=readme-ov-file#-contribution'}>
+                <DropdownMenuItem
+                  onClick={() =>
+                    (window.location.href =
+                      "https://github.com/dMario24/flag123?tab=readme-ov-file#-contribution")
+                  }
+                >
                   <Github />
                   <span>Contribution</span>
                 </DropdownMenuItem>
@@ -223,14 +294,93 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
                 <DropdownMenuItem>
                   <LogOut />
                   <span>나가기</span>
-                  {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
                 </DropdownMenuItem>
               </DropdownMenuContent>
-            </DropdownMenu>
+            </DropdownMenu> */}
+
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Menu
+                  size={33}
+                  // className="text-red-500"
+                />
+              </DrawerTrigger>
+              <DrawerContent>
+                <div className="mx-auto w-full max-w-sm">
+                  <DrawerHeader>
+                    <DrawerTitle>
+                      12.3 계엄배 천하제일 재기발랄 깃발대회
+                    </DrawerTitle>
+                    <DrawerDescription>
+                      민주주의 해방전선 나만 깃발 없엉 총연맹
+                    </DrawerDescription>
+                  </DrawerHeader>
+                  <div className="p-4 pb-0">
+                    <div className="flex items-center justify-center space-x-2">
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="h-8 w-8 shrink-0 rounded-full"
+                        onClick={() => onClick(-10)}
+                        disabled={goal <= 200}
+                      >
+                        <Minus />
+                        <span className="sr-only">Decrease</span>
+                      </Button>
+                      <div className="flex-1 text-center">
+                        <div className="text-7xl font-bold tracking-tighter">
+                          {goal}
+                        </div>
+                        <div className="text-[0.70rem] uppercase text-muted-foreground">
+                          Calories/day
+                        </div>
+                      </div>
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="h-8 w-8 shrink-0 rounded-full"
+                        onClick={() => onClick(10)}
+                        disabled={goal >= 400}
+                      >
+                        <Plus />
+                        <span className="sr-only">Increase</span>
+                      </Button>
+                    </div>
+                    <div className="mt-3 h-[120px]">
+                      <ResponsiveContainer width="100%" height="100%">
+                        <BarChart data={data}>
+                          <Bar
+                            dataKey="goal"
+                            style={
+                              {
+                                fill: "hsl(var(--foreground))",
+                                opacity: 0.9,
+                              } as React.CSSProperties
+                            }
+                          />
+                        </BarChart>
+                      </ResponsiveContainer>
+                    </div>
+                  </div>
+                  <DrawerFooter>
+                    <Button onClick={() => sortFlags("desc")}>DESC</Button>
+                    <Button onClick={() => sortFlags("asc")}>ASC</Button>
+                    <Button onClick={() => sortFlags("shuffle")}>SHUFFLE</Button>
+                    <DrawerClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                  </DrawerFooter>
+                </div>
+              </DrawerContent>
+            </Drawer>
 
             <h1 className="text-xl font-bold text-green-300 flex space-x-1">
-              <span className="text-indigo-400 hidden lg:inline">12.3 계엄배</span>
-              <span className="text-indigo-200 hidden md:inline">천하제일 깃발대회</span>
+              <span className="text-indigo-400 hidden lg:inline">
+                12.3 계엄배
+              </span>
+              <span className="text-indigo-200 hidden md:inline">
+                천하제일 깃발대회
+              </span>
             </h1>
           </div>
 
@@ -268,26 +418,31 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
                   {/* 좋아요 버튼과 숫자 (이미지 좌측 하단) */}
                   {flag.like_count > -1 &&
                     (process.env.NEXT_PUBLIC_LIKE_BUTTON_ENABLED || "OFF") ===
-                    "ON" && (
+                      "ON" && (
                       <div className="absolute bottom-2 left-2 flex items-center gap-1">
                         <button
                           onClick={() => toggleLike(String(flag.id))}
-                          className={`flex items-center justify-center w-7 h-7 rounded-full ${likedFlags.includes(String(flag.id))
-                            ? "bg-red-500 text-white"
-                            : "bg-gray-700 text-gray-300"
-                            }`}
+                          className={`flex items-center justify-center w-7 h-7 rounded-full ${
+                            likedFlags.includes(String(flag.id))
+                              ? "bg-red-500 text-white"
+                              : "bg-gray-700 text-gray-300"
+                          }`}
                         >
                           <Heart
-                            className={`w-5 h-5 transition-transform duration-500 ${animatingFlags[flag.id] ? "animate-heartbeat" : "scale-100"
-                              }`}
+                            className={`w-5 h-5 transition-transform duration-500 ${
+                              animatingFlags[flag.id]
+                                ? "animate-heartbeat"
+                                : "scale-100"
+                            }`}
                           />
                         </button>
                         {/* 말풍선 부분 */}
-                        <div className="relative flex items-center ml-2"> {/* ml-2로 말풍선 위치 조정 */}
+                        <div className="relative flex items-center ml-2">
+                          {" "}
+                          {/* ml-2로 말풍선 위치 조정 */}
                           <span className="text-white bg-blue-500 text-sm px-3 py-1 rounded-lg relative z-0">
                             {flag.like_count}
                           </span>
-
                           {/* 말풍선 꼬리 */}
                           <div className="absolute top-1/2 left-0 transform -translate-x-full -translate-y-1/2 w-0 h-0 border-t-8 border-b-8 border-r-8 border-t-transparent border-b-transparent border-r-blue-500"></div>
                         </div>
@@ -297,23 +452,23 @@ export default function FlagsPage({ initialFlags }: FlagsProps) {
                   {/* MapPinned 버튼 (환경 변수로 ON/OFF) */}
                   {(process.env.NEXT_PUBLIC_MAP_PINNED_ENABLED || "OFF") ===
                     "ON" && (
-                      <button
-                        onClick={() =>
-                          console.log(`MapPinned clicked for ${flag.id}`)
-                        }
-                        className="absolute bottom-2 right-2 flex items-center justify-center w-7 h-7 rounded-full bg-gray-700 text-white hover:bg-blue-600"
-                      >
-                        <Info className="w-5 h-5" />
-                      </button>
-                    )}
+                    <button
+                      onClick={() =>
+                        console.log(`MapPinned clicked for ${flag.id}`)
+                      }
+                      className="absolute bottom-2 right-2 flex items-center justify-center w-7 h-7 rounded-full bg-gray-700 text-white hover:bg-blue-600"
+                    >
+                      <Info className="w-5 h-5" />
+                    </button>
+                  )}
                 </div>
                 {/* 플래그 이름 */}
-                <p className="mt-2 text-pretty font-bold" > {flag.name}</p>
+                <p className="mt-2 text-pretty font-bold"> {flag.name}</p>
               </li>
             ))}
           </ul>
         </section>
-      </main >
-    </div >
+      </main>
+    </div>
   );
 }
